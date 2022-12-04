fc2dd3b627e5ca5734968be3b49b7a2ad6ed77a2
saulitis <saulitis3@gmail.com>
2022-12-04 19:16:59 +0200

    First challange of day4 completed

---
+++ day4/src/main.rs
@@ -0,0 +1,10 @@
+mod first_challange;
+
+pub use utils;
+
+use crate::first_challange::part1;
+
+fn main() {
+    println!("Answer to the first challange: {}", part1());
+    //println!("Answer to the second challange: {}", part2());
+}
